2b45e7aca2ae1c0d73fb4cc10b86973921437b01
Theplayer592 <lukaanddylan@gmail.com>
2022-03-05 18:31:55 +0000

    toolbar change

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 window.onload = function(e) {
-  //Wait for all of the files to load before removing the loading screen
-  //Because JavaScript takes so long to load an execute, this should make sure that everything has enough time to load
-  requestAnimationFrame(() => {
-    document.getElementById("loader").style.display = "none"
-    window.allLoaded = true
-  })
+    window.toolbarF = {
+        record: function() {
+            console.log("rec")
+        }
+    }
+
+    //Wait for all of the files to load before removing the loading screen
+    //Because JavaScript takes so long to load an execute, this should make sure that everything has enough time to load
+    requestAnimationFrame(() => {
+        document.getElementById("loader").style.display = "none"
+        window.allLoaded = true
+    })
 }
